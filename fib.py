2d3fbe123f33f2f6fddb91af2c8d91fdce06bb48
Katy Jendrzey <katyjendrzey@csm-wl-dhcp-236-79.mines.edu>
2019-11-21 11:33:09 -0700

    fibonacci added

---
+++ fib.py
@@ -0,0 +1,11 @@
+def fib():
+    fibs = [1, 2]
+    for i in range(1, 100):
+       fibs.append((fibs[i - 1] + fibs[i]))
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
